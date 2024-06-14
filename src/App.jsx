920d278caa95c65fcb01373221ab262f0e7317b9
ThenuThudewattage <127599146+ThenuThudewattage@users.noreply.github.com>
2024-06-14 16:11:38 +0530

    create header components

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,23 @@ import Home from './pages/Home'
 import Signin from './pages/Signin'
 import SignUp from './pages/SignUp'
 import Profile from './pages/Profile'
-import About from './pages/About'
+import About from './pages/About' // Change the import statement to './pages/about'
+import Header from './components/Header';
 
 export default function App() {
-  return <BrowserRouter>
-  <Routes>
-    <Route path = "/" element = {<Home />} />
-    <Route path = "/sign-in" element = {<Signin />} />
-    <Route path = "/sign-up" element = {<SignUp />} />
-    <Route path = "/about" element = {<About />} />
-    <Route path = "/profile" element = {<Profile />} />
+  return(
+    <BrowserRouter>
+    <Header/>
+      <Routes>
+        <Route path = "/" element = {<Home />} />
+        <Route path = "/sign-in" element = {<Signin />} />
+        <Route path = "/sign-up" element = {<SignUp />} />
+        <Route path = "/about" element = {<About />} />
+        <Route path = "/profile" element = {<Profile />} />
 
-  </Routes>
+      </Routes>
 
   </BrowserRouter>
+  ); 
+  
 }
