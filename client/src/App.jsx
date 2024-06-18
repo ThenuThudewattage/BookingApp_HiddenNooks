93d781c419e3bf78f2954c3409406f828efee0a5
ThenuThudewattage <127599146+ThenuThudewattage@users.noreply.github.com>
2024-06-18 14:33:16 +0530

    add redux toolkit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import React from 'react'
 import Home from './pages/Home'
 import Signin from './pages/Signin'
 import SignUp from './pages/SignUp'
