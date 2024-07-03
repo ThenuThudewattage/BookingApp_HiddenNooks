ff6e1d6f0fa2574e055b4d3a78fa021644baa0b7
ThenuThudewattage <127599146+ThenuThudewattage@users.noreply.github.com>
2024-07-04 00:18:28 +0530

    completed ffirst phase

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,28 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4
+      text-slate-800'>About HiddenNooks</h1>
+      <p className='mb-4 text-slate-700'>Welcome to HiddenNooks, your gateway to discovering the world's 
+        best-kept secret getaways. At HiddenNooks, we believe that the perfect 
+        retreat lies off the beaten path, nestled in charming corners and serene 
+        hideaways waiting to be explored.</p>
+      
+      <p className='mb-4 text-slate-700'>
+        Our mission is to connect travelers with unique and cozy accommodations
+        that provide a true escape from the hustle and bustle of everyday life. 
+        Whether you're seeking a rustic cabin in the woods, a quaint cottage by the 
+        sea, or a tranquil mountain retreat, HiddenNooks offers a curated selection 
+        of hidden gems that promise unforgettable experiences.
+      </p>
+
+      <p className='mb-4 text-slate-700'>
+        With HiddenNooks, you can discover and book one-of-a-kind accommodations 
+        that cater to your individual tastes and preferences. From romantic 
+        getaways to family vacations, solo adventures to group retreats, our 
+        diverse range of listings ensures that there's something for everyone.
+      </p>
+    </div>
   )
 }
